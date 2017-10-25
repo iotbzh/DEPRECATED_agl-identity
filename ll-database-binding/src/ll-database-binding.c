c063b14530bad81d664a4505742d20d39cdb130f
José Bollo <jose.bollo@iot.bzh>
2017-10-25 16:10:49 +0200

    Revert "database: update the name of the API to 'lludb'"
    This reverts commit d47da1579ad3912af32377dad1e361e4659e9be6.

--- ll-database-binding/src/ll-database-binding.c
+++ ll-database-binding/src/ll-database-binding.c
@@ -452,7 +452,7 @@ static const afb_verb_v2 ll_database_binding_verbs[]= {
 };
 
 const struct afb_binding_v2 afbBindingV2 = {
-	.api = "lludb", /* low-level user database */
+	.api = "ll-database",
 	.specification = NULL,
 	.verbs = ll_database_binding_verbs,
 	.preinit = NULL,
